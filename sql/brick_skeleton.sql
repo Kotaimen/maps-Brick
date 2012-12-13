2340041dd13a3791c62f9f37994382e4103c07a1
Kotaimen.Cao <kotaimen.c@gmail.com>
2012-12-13 23:13:34 +0800

    Update map theme

--- sql/brick_skeleton.sql
+++ sql/brick_skeleton.sql
@@ -6,11 +6,10 @@ DROP TABLE IF EXISTS "brick_skeleton";
 
 CREATE TABLE brick_skeleton AS 
 	SELECT 
-		osm_id,
 		( CASE 
 			WHEN ref ~ E'^I ?[[:digit:]]+' THEN regexp_replace(ref, '^I ?([[:digit:]]+).*', E'\\1')
 			WHEN ref ~ E'^US ?[[:digit:]]+' THEN regexp_replace(ref, '^US ?([[:digit:]]+).*', E'\\1')
-			WHEN ref ~ E'^[[:alpha:]]+ ?[[:alnum:]]+' THEN regexp_replace(ref, '^([[:alpha:]]+) ?([[:alnum:]]+).*', E'\\1\\2')
+			WHEN ref ~ E'^[[:alpha:]]+ ?[[:alpha:]]?[[:digit:]]+' THEN regexp_replace(ref, '^([[:alpha:]]+) ?([[:alnum:]]+).*', E'\\1\\2')
 			ELSE NULL
 		END ) AS ref,
 		( CASE 
@@ -18,11 +17,13 @@ CREATE TABLE brick_skeleton AS
 			WHEN ref ~ E'^US ?[[:digit:]]+' THEN 'US/US'
 			WHEN CHAR_LENGTH(ref) <=5 THEN 'US/STATE'
 			ELSE NULL
-		END) AS category,
-		st_simplifypreservetopology(way, 50) AS way
+		END) AS category, 
+		--st_simplify(way, 6) as way
+		ST_Segmentize(ST_SimplifyPreserveTopology(ST_LineMerge(st_collect(way)), 6), 5000) AS way
 	FROM planet_osm_line
 		WHERE highway IN ('motorway', 'trunk')
-	ORDER BY z_order
+	GROUP BY ref
+	--ORDER BY z_order
 ;
 
 CREATE INDEX ON brick_skeleton USING gist(way);
