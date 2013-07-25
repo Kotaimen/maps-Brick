3939e23ec05de3489597f39fccee45766630e259
ray <gliese.q@gmail.com>
2013-07-25 10:17:10 +0800

    remove platform from railway

--- sql/brick_road.sql
+++ sql/brick_road.sql
@@ -2,91 +2,77 @@ BEGIN;
 
 
 
-
 CREATE OR REPLACE VIEW brick_road AS 
-SELECT
-    osm_id,
-	road_class,
-	road_type,
-	regexp_replace(foo.name, '(.*)\(.*\)' , '\1' ) AS road_name, 
-	ref,
-	CASE
-		WHEN foo.name ~* '\mavenue$'  THEN regexp_replace(foo.name, '\m(ave)nue$' , '\1' , 'i' )
-		WHEN foo.name ~* '\mboulevard$'  THEN regexp_replace(foo.name, '\m(b)oulevard$' , '\1lvd' , 'i' )
-		WHEN foo.name ~* '\mexpressway$'  THEN regexp_replace(foo.name, '\m(E)xpressway$' , '\1xpwy' , 'i' )
-		WHEN foo.name ~* '\mhighway$'  THEN regexp_replace(foo.name, '\m(h)ighway$' , '\1wy' , 'i' )
-		WHEN foo.name ~* '\mparkway$'  THEN regexp_replace(foo.name, '\m(p)arkway$' , '\1kwy' , 'i' )
-		WHEN foo.name ~* '\mcourt$'  THEN regexp_replace(foo.name, '\m(c)ourt$' , '\1t' , 'i' )
-		WHEN foo.name ~* '\mdrive$'  THEN regexp_replace(foo.name, '\m(dr)ive$' , '\1' , 'i' )
-		WHEN foo.name ~* '\mplace$'  THEN regexp_replace(foo.name, '\m(pl)ace$' , '\1' , 'i' )
-		WHEN foo.name ~* '\mlane$'  THEN regexp_replace(foo.name, '\m(l)ane$' , '\1n' , 'i' )
-		WHEN foo.name ~* '\mroad$'  THEN regexp_replace(foo.name, '\m(r)oad$' , '\1d' , 'i' )
-		WHEN foo.name ~* '\mstreet$'  THEN regexp_replace(foo.name, '\m(st)reet$' , '\1' , 'i' )
-		WHEN foo.name ~* '\mtrail$'  THEN regexp_replace(foo.name, '\m(tr)ail$' , '\1' , 'i' )
-		WHEN foo.name ~* '\mway$'  THEN regexp_replace(foo.name, '\m(w)ay$' , '\1y' , 'i' )
-		ELSE foo.name
-	END AS road_name_abbr, 
-	CASE
-		WHEN foo.oneway = ANY (ARRAY['yes' , 'true' ]) THEN 1
-		WHEN foo.oneway = ANY (ARRAY['no' , 'false' ]) THEN 0
-		WHEN foo.oneway = '-1'  THEN (-1)
-		ELSE 0
-	END AS is_oneway, 
-	CASE
-		WHEN foo.highway ~~ '%_link'  THEN 1
-		ELSE 0
-	END AS is_link, 
-	CASE
-		WHEN foo.tunnel = ANY (ARRAY['yes' , 'true' ]) THEN 1
-		ELSE 0
-	END AS is_tunnel, 
-	CASE
-		WHEN foo.bridge = ANY (ARRAY['yes' , 'true' ]) THEN 1
-		ELSE 0
-	END AS is_bridge, 
-	CASE
-		WHEN foo.layer ~ '^-?[[:digit:]]+(\.[[:digit:]]+)?$'  THEN foo.layer::double precision
-		ELSE 0::double precision
-	END AS explicit_layer, 
-	CASE
-		WHEN foo.tunnel = ANY (ARRAY['yes' , 'true' ]) THEN (-1)
-		WHEN foo.bridge = ANY (ARRAY['yes' , 'true' ]) THEN 1
-		ELSE 0
-	END AS implied_layer, 
-	CASE
-		WHEN foo.highway = 'motorway'  THEN 0::numeric
-		WHEN foo.railway IS NOT NULL THEN 0.5
-		WHEN foo.highway = 'trunk'  THEN 1::numeric
-		WHEN foo.highway = 'primary'  THEN 2::numeric
-		WHEN foo.highway = 'secondary'  THEN 3::numeric
-		WHEN foo.highway = 'tertiary'  THEN 4::numeric
-		WHEN foo.highway ~~ '%_link'  THEN 5::numeric
-		WHEN foo.highway = ANY (ARRAY['residential' , 'unclassified' , 'road' ]) THEN 6::numeric
-		WHEN foo.highway = ANY (ARRAY['service' , 'minor' ]) THEN 7::numeric
-		ELSE 99::numeric
-	END AS priority,
-    way
-FROM 
-	(         
-	SELECT 
-		CASE
-			WHEN highway = ANY (ARRAY['motorway', 'motorway_link']) THEN 'highway'
-			WHEN highway = ANY (ARRAY['trunk', 'trunk_link']) THEN 'trunk'
-			WHEN highway = ANY (ARRAY['primary', 'primary_link', 'secondary', 'secondary_link', 'tertiary', 'tertiary_link']) THEN 'major_road'
-			WHEN highway = ANY (ARRAY['residential', 'unclassified', 'road', 'minor']) THEN 'minor_road'
-			WHEN highway = ANY (ARRAY['service', 'footpath', 'track', 'footway', 'steps', 'pedestrian', 'path', 'cycleway', 'living_street']) THEN 'path'
-			ELSE NULL
-		END AS road_class, 
-		highway AS road_type,
-		osm_id, name, ref, highway, railway, oneway, bridge, tunnel, layer, way
-		FROM planet_osm_line
-		WHERE highway = ANY (ARRAY['motorway', 'motorway_link', 'trunk', 'trunk_link', 'primary', 'primary_link', 'secondary', 'secondary_link', 'tertiary', 'tertiary_link', 'residential', 'unclassified', 'road', 'minor', 'service', 'footpath', 'track', 'footway', 'steps', 'pedestrian', 'path', 'cycleway', 'living_street'])
-	UNION ALL 
-		SELECT 'rail' AS road_class, railway AS road_type, osm_id, name, ref, highway, railway, oneway, bridge, tunnel, layer, way
-		FROM planet_osm_line
-		WHERE railway IS NOT NULL
-	) foo;
-	
+ SELECT planet_osm_line.osm_id, 
+        CASE
+            WHEN planet_osm_line.highway = ANY (ARRAY['motorway'::text, 'motorway_link'::text]) THEN 'highway'::text
+            WHEN planet_osm_line.highway = ANY (ARRAY['trunk'::text, 'trunk_link'::text]) THEN 'trunk'::text
+            WHEN planet_osm_line.highway = ANY (ARRAY['primary'::text, 'primary_link'::text, 'secondary'::text, 'secondary_link'::text, 'tertiary'::text, 'tertiary_link'::text]) THEN 'major_road'::text
+            WHEN planet_osm_line.highway = ANY (ARRAY['residential'::text, 'unclassified'::text, 'road'::text, 'minor'::text]) THEN 'minor_road'::text
+            WHEN planet_osm_line.highway = ANY (ARRAY['service'::text, 'footpath'::text, 'track'::text, 'footway'::text, 'steps'::text, 'pedestrian'::text, 'path'::text, 'cycleway'::text, 'living_street'::text]) THEN 'path'::text
+            WHEN planet_osm_line.railway IS NOT NULL THEN 'rail'::text
+            ELSE NULL::text
+        END AS road_class,
+        COALESCE(planet_osm_line.highway, planet_osm_line.railway) AS road_type,
+        regexp_replace(planet_osm_line.name, '(.*)\(.*\)'::text, '\1'::text) AS road_name, 
+        CASE
+            WHEN planet_osm_line.name ~* '\mavenue$'::text THEN regexp_replace(planet_osm_line.name, '\m(ave)nue$'::text, '\1'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mboulevard$'::text THEN regexp_replace(planet_osm_line.name, '\m(b)oulevard$'::text, '\1lvd'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mexpressway$'::text THEN regexp_replace(planet_osm_line.name, '\m(E)xpressway$'::text, '\1xpwy'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mhighway$'::text THEN regexp_replace(planet_osm_line.name, '\m(h)ighway$'::text, '\1wy'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mparkway$'::text THEN regexp_replace(planet_osm_line.name, '\m(p)arkway$'::text, '\1kwy'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mcourt$'::text THEN regexp_replace(planet_osm_line.name, '\m(c)ourt$'::text, '\1t'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mdrive$'::text THEN regexp_replace(planet_osm_line.name, '\m(dr)ive$'::text, '\1'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mplace$'::text THEN regexp_replace(planet_osm_line.name, '\m(pl)ace$'::text, '\1'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mlane$'::text THEN regexp_replace(planet_osm_line.name, '\m(l)ane$'::text, '\1n'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mroad$'::text THEN regexp_replace(planet_osm_line.name, '\m(r)oad$'::text, '\1d'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mstreet$'::text THEN regexp_replace(planet_osm_line.name, '\m(st)reet$'::text, '\1'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mtrail$'::text THEN regexp_replace(planet_osm_line.name, '\m(tr)ail$'::text, '\1'::text, 'i'::text)
+            WHEN planet_osm_line.name ~* '\mway$'::text THEN regexp_replace(planet_osm_line.name, '\m(w)ay$'::text, '\1y'::text, 'i'::text)
+            ELSE planet_osm_line.name
+        END AS road_name_abbr, 
+        CASE
+            WHEN planet_osm_line.oneway = ANY (ARRAY['yes'::text, 'true'::text]) THEN 1
+            WHEN planet_osm_line.oneway = ANY (ARRAY['no'::text, 'false'::text]) THEN 0
+            WHEN planet_osm_line.oneway = '-1'::text THEN (-1)
+            ELSE 0
+        END AS is_oneway, 
+        CASE
+            WHEN planet_osm_line.highway ~~ '%_link'::text THEN 1
+            ELSE 0
+        END AS is_link, 
+        CASE
+            WHEN planet_osm_line.tunnel = ANY (ARRAY['yes'::text, 'true'::text]) THEN 1
+            ELSE 0
+        END AS is_tunnel, 
+        CASE
+            WHEN planet_osm_line.bridge = ANY (ARRAY['yes'::text, 'true'::text]) THEN 1
+            ELSE 0
+        END AS is_bridge, 
+        CASE
+            WHEN planet_osm_line.layer ~ '^-?[[:digit:]]+(\.[[:digit:]]+)?$'::text THEN planet_osm_line.layer::double precision
+            ELSE 0::double precision
+        END AS explicit_layer, 
+        CASE
+            WHEN planet_osm_line.tunnel = ANY (ARRAY['yes'::text, 'true'::text]) THEN (-1)
+            WHEN planet_osm_line.bridge = ANY (ARRAY['yes'::text, 'true'::text]) THEN 1
+            ELSE 0
+        END AS implied_layer, 
+        CASE
+            WHEN planet_osm_line.highway = 'motorway'::text THEN 0::numeric
+            WHEN planet_osm_line.railway IS NOT NULL THEN 0.5
+            WHEN planet_osm_line.highway = 'trunk'::text THEN 1::numeric
+            WHEN planet_osm_line.highway = 'primary'::text THEN 2::numeric
+            WHEN planet_osm_line.highway = 'secondary'::text THEN 3::numeric
+            WHEN planet_osm_line.highway = 'tertiary'::text THEN 4::numeric
+            WHEN planet_osm_line.highway ~~ '%_link'::text THEN 5::numeric
+            WHEN planet_osm_line.highway = ANY (ARRAY['residential'::text, 'unclassified'::text, 'road'::text]) THEN 6::numeric
+            WHEN planet_osm_line.highway = ANY (ARRAY['service'::text, 'minor'::text]) THEN 7::numeric
+            ELSE 99::numeric
+        END AS priority, planet_osm_line.way
+   FROM planet_osm_line
+  WHERE (planet_osm_line.highway = ANY (ARRAY['motorway'::text, 'motorway_link'::text, 'trunk'::text, 'trunk_link'::text, 'primary'::text, 'primary_link'::text, 'secondary'::text, 'secondary_link'::text, 'tertiary'::text, 'tertiary_link'::text, 'residential'::text, 'unclassified'::text, 'road'::text, 'minor'::text, 'service'::text, 'footpath'::text, 'track'::text, 'footway'::text, 'steps'::text, 'pedestrian'::text, 'path'::text, 'cycleway'::text, 'living_street'::text])) OR (planet_osm_line.railway IS NOT NULL AND planet_osm_line.railway != 'platform');
+
 
 CREATE OR REPLACE VIEW brick_road_gen0 AS 
 	SELECT * FROM brick_road;
