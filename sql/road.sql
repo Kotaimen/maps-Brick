03e43d84d401a63f19ab1d577b396fbfd1923478
GlieseRay <gliese.q@gmail.com>
2013-11-14 02:36:28 -0800

    update road view

--- sql/road.sql
+++ sql/road.sql
@@ -9,6 +9,20 @@ CREATE INDEX ON osm_roads(bridge);
 CREATE INDEX ON osm_roads(oneway);
 CREATE INDEX ON osm_roads(layer);
 
+CREATE INDEX ON osm_roads_gen0(type);
+CREATE INDEX ON osm_roads_gen0(class);
+CREATE INDEX ON osm_roads_gen0(tunnel);
+CREATE INDEX ON osm_roads_gen0(bridge);
+CREATE INDEX ON osm_roads_gen0(oneway);
+CREATE INDEX ON osm_roads_gen0(layer);
+
+CREATE INDEX ON osm_roads_gen1(type);
+CREATE INDEX ON osm_roads_gen1(class);
+CREATE INDEX ON osm_roads_gen1(tunnel);
+CREATE INDEX ON osm_roads_gen1(bridge);
+CREATE INDEX ON osm_roads_gen1(oneway);
+CREATE INDEX ON osm_roads_gen1(layer);
+
 
 CREATE OR REPLACE VIEW import.brick_roads_stage AS 
     SELECT osm_roads.id, osm_roads.osm_id, osm_roads.class, osm_roads.type, osm_roads.tunnel, osm_roads.bridge, osm_roads.oneway, 
@@ -34,6 +48,56 @@ CREATE OR REPLACE VIEW import.brick_roads_stage AS
     FROM import.osm_roads;
 
 
+CREATE OR REPLACE VIEW import.brick_roads_stage_gen0 AS 
+ SELECT osm_roads_gen0.osm_id, osm_roads_gen0.class, osm_roads_gen0.type, osm_roads_gen0.tunnel, osm_roads_gen0.bridge, osm_roads_gen0.oneway, 
+        CASE
+            WHEN osm_roads_gen0.type::text ~~ '%_link'::text THEN 1::smallint
+            ELSE 0::smallint
+        END AS link, osm_roads_gen0.access, osm_roads_gen0.service, 
+        CASE
+            WHEN osm_roads_gen0.layer IS NULL THEN 0::smallint
+            ELSE osm_roads_gen0.layer::smallint
+        END AS layer, 
+        CASE
+            WHEN osm_roads_gen0.type::text = 'motorway'::text THEN 0::smallint
+            WHEN osm_roads_gen0.type::text = 'trunk'::text THEN 1::smallint
+            WHEN osm_roads_gen0.class::text = 'railway'::text THEN 2::smallint
+            WHEN osm_roads_gen0.type::text = 'primary'::text THEN 3::smallint
+            WHEN osm_roads_gen0.type::text = 'secondary'::text THEN 4::smallint
+            WHEN osm_roads_gen0.type::text = 'tertiary'::text THEN 5::smallint
+            WHEN osm_roads_gen0.type::text ~~ '%_link'::text THEN 6::smallint
+            WHEN osm_roads_gen0.type::text = ANY (ARRAY['residential'::character varying::text, 'unclassified'::character varying::text, 'road'::character varying::text, 'minor'::character varying::text]) THEN 7::smallint
+            ELSE 8::smallint
+        END AS rank, osm_roads_gen0.geometry
+   FROM import.osm_roads_gen0;
+
+
+CREATE OR REPLACE VIEW import.brick_roads_stage_gen1 AS 
+ SELECT osm_roads_gen1.osm_id, osm_roads_gen1.class, osm_roads_gen1.type, osm_roads_gen1.tunnel, osm_roads_gen1.bridge, osm_roads_gen1.oneway, 
+        CASE
+            WHEN osm_roads_gen1.type::text ~~ '%_link'::text THEN 1::smallint
+            ELSE 0::smallint
+        END AS link, osm_roads_gen1.access, osm_roads_gen1.service, 
+        CASE
+            WHEN osm_roads_gen1.layer IS NULL THEN 0::smallint
+            ELSE osm_roads_gen1.layer::smallint
+        END AS layer, 
+        CASE
+            WHEN osm_roads_gen1.type::text = 'motorway'::text THEN 0::smallint
+            WHEN osm_roads_gen1.type::text = 'trunk'::text THEN 1::smallint
+            WHEN osm_roads_gen1.class::text = 'railway'::text THEN 2::smallint
+            WHEN osm_roads_gen1.type::text = 'primary'::text THEN 3::smallint
+            WHEN osm_roads_gen1.type::text = 'secondary'::text THEN 4::smallint
+            WHEN osm_roads_gen1.type::text = 'tertiary'::text THEN 5::smallint
+            WHEN osm_roads_gen1.type::text ~~ '%_link'::text THEN 6::smallint
+            WHEN osm_roads_gen1.type::text = ANY (ARRAY['residential'::character varying::text, 'unclassified'::character varying::text, 'road'::character varying::text, 'minor'::character varying::text]) THEN 7::smallint
+            ELSE 8::smallint
+        END AS rank, osm_roads_gen1.geometry
+   FROM import.osm_roads_gen1;
+
+
+
+
 CREATE OR REPLACE VIEW import.brick_roads AS 
     SELECT * FROM (
 	SELECT 'outline'::text AS render, 1 AS outline, 0 AS render_order, * FROM import.brick_roads_stage
@@ -53,44 +117,41 @@ CREATE OR REPLACE VIEW import.brick_roads AS
 	  foo.rank DESC;
 
 
+
 CREATE OR REPLACE VIEW import.brick_roads_gen0 AS 
-    SELECT brick_roads_stage.id, 
-    	   brick_roads_stage.osm_id, 
-	   brick_roads_stage.class, 
-	   brick_roads_stage.type, 
-	   brick_roads_stage.tunnel, 
-	   brick_roads_stage.bridge, 
-	   brick_roads_stage.oneway, 
-	   brick_roads_stage.link, 
-	   brick_roads_stage.access, 
-	   brick_roads_stage.service, 
-	   brick_roads_stage.layer, 
-	   brick_roads_stage.rank, 
-	   brick_roads_stage.geometry
-    FROM import.brick_roads_stage
-    WHERE brick_roads_stage.type::text = ANY (ARRAY['motorway'::character varying::text, 'trunk'::character varying::text, 'primary'::character varying::text, 'secondary'::character varying::text])
-    ORDER BY brick_roads_stage.rank DESC;
+    SELECT brick_roads_stage_gen0.osm_id, 
+    	   brick_roads_stage_gen0.class, 
+	   brick_roads_stage_gen0.type, 
+	   brick_roads_stage_gen0.tunnel, 
+	   brick_roads_stage_gen0.bridge, 
+	   brick_roads_stage_gen0.oneway, 
+	   brick_roads_stage_gen0.link, 
+	   brick_roads_stage_gen0.access, 
+	   brick_roads_stage_gen0.service, 
+	   brick_roads_stage_gen0.layer, 
+	   brick_roads_stage_gen0.rank, 
+	   brick_roads_stage_gen0.geometry
+    FROM import.brick_roads_stage_gen0
+    WHERE brick_roads_stage_gen0.type::text = ANY (ARRAY['motorway'::character varying::text, 'trunk'::character varying::text, 'primary'::character varying::text])
+    ORDER BY brick_roads_stage_gen0.rank DESC;
+
 
 
 CREATE OR REPLACE VIEW import.brick_roads_gen1 AS 
-    SELECT brick_roads.render, 
-    	   brick_roads.outline, 
-	   brick_roads.render_order, 
-	   brick_roads.id, 
-	   brick_roads.osm_id, 
-	   brick_roads.class, 
-	   brick_roads.type, 
-	   brick_roads.tunnel, 
-	   brick_roads.bridge, 
-	   brick_roads.oneway, 
-	   brick_roads.link, 
-	   brick_roads.access, 
-	   brick_roads.service, 
-	   brick_roads.layer, 
-	   brick_roads.rank, 
-	   brick_roads.geometry
-   FROM import.brick_roads
-   WHERE (brick_roads.type::text = ANY (ARRAY['motorway'::character varying, 'motorway_link'::character varying, 'trunk'::character varying, 'trunk_link'::character varying, 'primary'::character varying, 'primary_link'::character varying, 'secondary'::character varying, 'secondary_link'::character varying, 'tertiary'::character varying, 'tertiary_link'::character varying, 'rail'::character varying]::text[])) AND render IN ('casing'::character varying, 'inline'::character varying) ;
+    SELECT *
+    FROM (         
+    	SELECT 'casing'::text AS render, 1 AS outline, 0 AS render_order, * FROM import.brick_roads_stage_gen1
+        UNION ALL 
+        SELECT 'inline'::text AS render, 0 AS outline, 1 AS render_order, * FROM import.brick_roads_stage_gen1
+    ) foo
+    WHERE foo.type::text = ANY (ARRAY['motorway'::character varying::text, 'motorway_link'::character varying::text, 'trunk'::character varying::text, 'trunk_link'::character varying::text, 'primary'::character varying::text, 'primary_link'::character varying::text, 'secondary'::character varying::text, 'secondary_link'::character varying::text, 'tertiary'::character varying::text, 'tertiary_link'::character varying::text, 'rail'::character varying::text])
+    ORDER BY foo.outline DESC, 
+    	     foo.layer ASC, 
+	     foo.tunnel DESC NULLS LAST, 
+	     foo.bridge NULLS FIRST, 
+	     foo.render_order ASC, 
+	     foo.rank DESC;
+
 
 
 COMMIT;
