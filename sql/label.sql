a908443e29328cc21bd35766f62832130ddd367f
GlieseRay <gliese.q@gmail.com>
2013-11-14 01:59:29 -0800

    add landusage labels

--- sql/label.sql
+++ sql/label.sql
@@ -22,4 +22,19 @@ CREATE OR REPLACE VIEW import.brick_places AS
     FROM osm_places
     ORDER BY rank, population DESC NULLS LAST;
 
+
+
+CREATE OR REPLACE VIEW import.brick_landusage_area_labels AS 
+    SELECT brick_landusage_areas.id, 
+    	   brick_landusage_areas.osm_id, 
+	   brick_landusage_areas.name, 
+	   brick_landusage_areas.class, 
+	   brick_landusage_areas.type, 
+	   brick_landusage_areas.area, 
+	   st_pointonsurface(brick_landusage_areas.geometry) AS geometry
+    FROM import.brick_landusage_areas
+    WHERE brick_landusage_areas.name IS NOT NULL AND brick_landusage_areas.name::text <> ''::text
+    ORDER BY brick_landusage_areas.area DESC;
+
+
 COMMIT;
