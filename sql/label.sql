d2d2c0e28461a3a49fcc14267801cf999bf2d995
Kotaimen.Cao <kotaimen.c@gmail.com>
2013-11-16 14:33:18 +0800

    Add road label gen1 and partial index

--- sql/label.sql
+++ sql/label.sql
@@ -1,4 +1,4 @@
-BEGIN;
+﻿BEGIN;
 
 SET search_path TO import, public;
 
@@ -8,7 +8,18 @@ CREATE INDEX ON osm_places(population);
 CREATE INDEX ON osm_landusage_areas(name);
 CREATE INDEX ON osm_landusage_areas(area);
 
+CREATE INDEX ON osm_landusage_areas USING gist(st_centroid(geometry)) 
+WHERE area > 1024 AND area <= 10240000  AND name IS NOT NULL AND name <> '' AND st_isvalid(geometry);
+
+CREATE INDEX ON osm_landusages_gen0 USING gist(st_pointonsurface(geometry)) 
+WHERE area > 1024000000 AND name IS NOT NULL AND name <> '' AND st_isvalid(geometry);
+
+CREATE INDEX ON osm_landusages_gen1 USING gist(st_pointonsurface(geometry)) 
+WHERE area > 10240000 AND name IS NOT NULL AND name <> '' AND st_isvalid(geometry);
+
+
 CREATE INDEX ON osm_roads(name);
+CREATE INDEX ON osm_roads_gen1(name);
 
 
 CREATE OR REPLACE VIEW import.brick_places AS
@@ -28,19 +39,16 @@ CREATE OR REPLACE VIEW import.brick_places AS
     FROM osm_places
     ORDER BY rank, population DESC NULLS LAST;
 
-
-
-
 CREATE OR REPLACE VIEW import.brick_landusage_area_labels AS 
-    SELECT id, osm_id, name, class, type, area, 
-	st_pointonsurface(geometry)::geometry(Point,3857) AS geometry
+    SELECT osm_id, name, class, type, area, 
+        st_centroid(geometry)::geometry(Point,3857) AS geometry
     FROM import.osm_landusage_areas
-    WHERE area > 1024::double precision AND name IS NOT NULL AND name::text <> ''::text AND st_isvalid(geometry)
+    WHERE area > 1024::double precision AND area <= 10240000 AND name IS NOT NULL AND name::text <> ''::text AND st_isvalid(geometry)
     ORDER BY area DESC;
 
 
 CREATE OR REPLACE VIEW import.brick_landusage_area_labels_gen0 AS 
-    SELECT id, osm_id, name, class, type, area, 
+    SELECT osm_id, name, class, type, area, 
 	st_pointonsurface(geometry)::geometry(Point,3857) AS geometry
     FROM import.osm_landusages_gen0
     WHERE area > 1024000000::double precision AND name IS NOT NULL AND name::text <> ''::text AND st_isvalid(geometry)
@@ -48,12 +56,11 @@ CREATE OR REPLACE VIEW import.brick_landusage_area_labels_gen0 AS
 
 
 CREATE OR REPLACE VIEW import.brick_landusage_area_labels_gen1 AS 
-    SELECT id, osm_id, name, class, type, area, 
+    SELECT osm_id, name, class, type, area, 
 	st_pointonsurface(geometry)::geometry(Point,3857) AS geometry
     FROM import.osm_landusages_gen1
     WHERE area > 10240000::double precision AND name IS NOT NULL AND name::text <> ''::text AND st_isvalid(geometry)
     ORDER BY area DESC;
-
 
 
 CREATE OR REPLACE VIEW import.brick_road_labels AS 
@@ -84,4 +91,31 @@ CREATE OR REPLACE VIEW import.brick_road_labels AS
            rank DESC;
 
 
+CREATE OR REPLACE VIEW import.brick_road_labels_gen1 AS 
+    SELECT osm_roads_gen1.class, osm_roads_gen1.type, regexp_replace(osm_roads_gen1.name::text, '(.*)\(.*\)'::text, '\1'::text) AS name, 
+            CASE
+                WHEN sin(pi() / 2::double precision - st_azimuth(st_startpoint(osm_roads_gen1.geometry), st_endpoint(osm_roads_gen1.geometry))) > 0::double precision THEN 1
+                ELSE (-1)
+            END AS direction, osm_roads_gen1.geometry,
+            CASE
+                WHEN osm_roads_gen1.type::text = 'motorway'::text THEN 0::smallint
+                WHEN osm_roads_gen1.type::text = 'motorway_link'::text THEN 1::smallint
+                WHEN osm_roads_gen1.type::text = 'trunk'::text THEN 2::smallint
+                WHEN osm_roads_gen1.type::text = 'trunk_link'::text THEN 3::smallint
+                WHEN osm_roads_gen1.type::text = 'primary'::text THEN 5::smallint
+                WHEN osm_roads_gen1.type::text = 'primary_link'::text THEN 6::smallint
+                WHEN osm_roads_gen1.type::text = 'secondary'::text THEN 7::smallint
+                WHEN osm_roads_gen1.type::text = 'secondary_link'::text THEN 8::smallint
+                WHEN osm_roads_gen1.type::text = 'tertiary'::text THEN 9::smallint
+                WHEN osm_roads_gen1.type::text = 'tertiary_link'::text THEN 10::smallint
+                WHEN osm_roads_gen1.type::text = ANY (ARRAY['residential'::character varying::text, 'unclassified'::character varying::text, 'road'::character varying::text, 'minor'::character varying::text]) THEN 11::smallint
+                WHEN osm_roads_gen1.class::text = 'railway'::text THEN 12::smallint
+                
+                ELSE 99::smallint
+            END AS rank
+       FROM import.osm_roads_gen1
+      WHERE osm_roads_gen1.name IS NOT NULL AND osm_roads_gen1.name::text <> ''::text
+      ORDER BY 
+           rank DESC;
+           
 COMMIT;
