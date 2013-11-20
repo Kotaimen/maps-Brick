8b7e5a59549b9d4979449ec4338533475ba09ec0
GlieseRay <gliese.q@gmail.com>
2013-11-20 01:46:26 -0800

    clean up label sql

--- sql/label.sql
+++ sql/label.sql
@@ -1,5 +1,5 @@
 ﻿BEGIN;
-
+    
 SET search_path TO import, public;
 
 CREATE INDEX ON osm_places(type);
@@ -9,157 +9,156 @@ CREATE INDEX ON osm_landusage_areas(area);
 
 CREATE INDEX ON landusage_area_labels(area);
 CREATE INDEX ON landusage_area_labels_gen0(area);
-CREATE INDEX ON	landusage_area_labels_gen1(area);
+CREATE INDEX ON landusage_area_labels_gen1(area);
 
 
-CREATE OR REPLACE VIEW import.brick_places AS
+CREATE OR REPLACE VIEW brick_places AS
     SELECT *,
-        CASE
-        	WHEN type = 'county' THEN 1
-        	WHEN type = 'region' THEN 2
-        	WHEN type = 'state' THEN 3
-        	WHEN type = 'city' THEN 4
-        	WHEN type = 'town' THEN 5
-        	WHEN type = 'suburb' THEN 6
-        	WHEN type = 'village' THEN 7
-        	WHEN type = 'hamlet' THEN 8
-        	WHEN type = 'locality' THEN 9
-        	ELSE 10
+        CASE WHEN type = 'county' THEN 1
+        	 WHEN type = 'region' THEN 2
+        	 WHEN type = 'state' THEN 3
+        	 WHEN type = 'city' THEN 4
+        	 WHEN type = 'town' THEN 5
+        	 WHEN type = 'suburb' THEN 6
+        	 WHEN type = 'village' THEN 7
+        	 WHEN type = 'hamlet' THEN 8
+        	 WHEN type = 'locality' THEN 9
+        	 ELSE 10
         END AS rank
     FROM osm_places
     ORDER BY rank, population DESC NULLS LAST;
 
-CREATE OR REPLACE VIEW import.brick_landusage_area_labels AS 
-    SELECT osm_id, name, class, type, area, 
-        st_centroid(geometry)::geometry(Point,3857) AS geometry
-    FROM import.landusages_area_labels
+
+CREATE OR REPLACE VIEW brick_landusage_area_labels AS 
+    SELECT osm_id, name, class, type, area, st_centroid(geometry)::geometry(Point,3857) AS geometry
+    FROM landusages_area_labels
     ORDER BY area DESC;
 
 
-CREATE OR REPLACE VIEW import.brick_landusage_area_labels_gen0 AS 
-    SELECT osm_id, name, class, type, area, 
-	st_pointonsurface(geometry)::geometry(Point,3857) AS geometry
-    FROM import.landusages_area_labels_gen0
+CREATE OR REPLACE VIEW brick_landusage_area_labels_gen0 AS 
+    SELECT osm_id, name, class, type, area, st_centroid(geometry)::geometry(Point,3857) AS geometry
+    FROM landusages_area_labels_gen0
     ORDER BY area DESC;
 
 
-CREATE OR REPLACE VIEW import.brick_landusage_area_labels_gen1 AS 
-    SELECT osm_id, name, class, type, area, 
-	st_pointonsurface(geometry)::geometry(Point,3857) AS geometry
-    FROM import.landusages_area_labels_gen1
+CREATE OR REPLACE VIEW brick_landusage_area_labels_gen1 AS 
+    SELECT osm_id, name, class, type, area, st_centroid(geometry)::geometry(Point,3857) AS geometry
+    FROM landusages_area_labels_gen1
     ORDER BY area DESC;
 
 
-CREATE OR REPLACE VIEW import.brick_road_labels AS 
+CREATE OR REPLACE VIEW brick_road_labels AS 
     SELECT * FROM (
-        SELECT osm_roads.class, osm_roads.type, regexp_replace(osm_roads.name::text, '(.*)\(.*\)'::text, '\1'::text) AS name, 
-            CASE
-                WHEN sin(pi() / 2 - st_azimuth(st_startpoint(osm_roads.geometry), st_endpoint(osm_roads.geometry))) > 0 THEN 1
-                ELSE (-1)
-            END AS direction, osm_roads.geometry,
-            CASE
-                WHEN osm_roads.type::text = 'motorway'::text THEN 0::smallint
-                WHEN osm_roads.type::text = 'trunk'::text THEN 2::smallint
-                WHEN osm_roads.type::text = 'primary'::text THEN 5::smallint
-                WHEN osm_roads.type::text = 'secondary'::text THEN 7::smallint
-                WHEN osm_roads.type::text = 'tertiary'::text THEN 9::smallint
-                WHEN osm_roads.type::text = ANY (ARRAY['residential'::character varying::text, 'unclassified'::character varying::text, 'road'::character varying::text, 'minor'::character varying::text]) THEN 11::smallint
-                WHEN osm_roads.class::text = 'railway'::text THEN 12::smallint
-            
-                ELSE 99::smallint
-            END AS rank,
-	    tunnel
-        FROM import.road_labels
+        SELECT  class, type, regexp_replace(name, '(.*)\(.*\)', '\1') AS name, 
+                CASE
+                    WHEN sin(pi() / 2 - st_azimuth(st_startpoint(geometry), st_endpoint(geometry))) > 0 THEN 1
+                    ELSE (-1)
+                END AS direction,
+                CASE
+                    WHEN type = 'motorway' THEN 0
+                    WHEN type = 'trunk' THEN 2
+                    WHEN type = 'primary' THEN 5
+                    WHEN type = 'secondary' THEN 7
+                    WHEN type = 'tertiary' THEN 9
+                    WHEN type = ANY (ARRAY['residential', 'unclassified', 'road', 'minor']) THEN 11
+                    WHEN class = 'railway' THEN 12
+                    ELSE 99
+                END AS rank,
+	            tunnel,
+	            geometry
+        FROM road_labels
         UNION ALL
-        SELECT 'ferry' AS class, 'ferry' AS type, osm_landusage_ways.name, 
-            CASE
-                WHEN sin(pi()/2 - st_azimuth(st_startpoint(osm_landusage_ways.geometry), st_endpoint(osm_landusage_ways.geometry))) > 0 THEN 1
-                ELSE (-1)
-            END AS direction,
-            osm_landusage_ways.geometry,
-            100::smallint as rank,
-	    0 AS tunnel 
+        SELECT 'ferry' AS class, 'ferry' AS type, name, 
+                CASE
+                    WHEN sin(pi()/2 - st_azimuth(st_startpoint(geometry), st_endpoint(geometry))) > 0 THEN 1
+                    ELSE (-1)
+                END AS direction,
+                100 as rank,
+	            0 AS tunnel,
+                geometry
         FROM osm_landusage_ways
         WHERE type='ferry'
-     ) AS foo
-      ORDER BY 
-           rank DESC;
+    ) AS foo
+    ORDER BY rank DESC;
 
-CREATE OR REPLACE VIEW import.brick_road_labels_gen1 AS 
-    SELECT osm_roads_gen1.class, osm_roads_gen1.type, regexp_replace(osm_roads_gen1.name::text, '(.*)\(.*\)'::text, '\1'::text) AS name, 
+
+CREATE OR REPLACE VIEW brick_road_labels_gen1 AS 
+    SELECT class, type, regexp_replace(name, '(.*)\(.*\)', '\1') AS name, 
             CASE
-                WHEN sin(pi() / 2 - st_azimuth(st_startpoint(osm_roads_gen1.geometry), st_endpoint(osm_roads_gen1.geometry))) > 0 THEN 1
+                WHEN sin(pi() / 2 - st_azimuth(st_startpoint(geometry), st_endpoint(geometry))) > 0 THEN 1
                 ELSE (-1)
-            END AS direction, osm_roads_gen1.geometry,
+            END AS direction,
             CASE
-                WHEN osm_roads_gen1.type::text = 'motorway'::text THEN 0::smallint
-                WHEN osm_roads_gen1.type::text = 'trunk'::text THEN 2::smallint
-                WHEN osm_roads_gen1.type::text = 'primary'::text THEN 5::smallint
-                WHEN osm_roads_gen1.type::text = 'secondary'::text THEN 7::smallint
-                WHEN osm_roads_gen1.type::text = 'tertiary'::text THEN 9::smallint
-                WHEN osm_roads_gen1.type::text = ANY (ARRAY['residential'::character varying::text, 'unclassified'::character varying::text, 'road'::character varying::text, 'minor'::character varying::text]) THEN 11::smallint
-                WHEN osm_roads_gen1.class::text = 'railway'::text THEN 12::smallint                
-                ELSE 99::smallint
+                WHEN type = 'motorway' THEN 0
+                WHEN type = 'trunk' THEN 2
+                WHEN type = 'primary' THEN 5
+                WHEN type = 'secondary' THEN 7
+                WHEN type = 'tertiary' THEN 9
+                WHEN type = ANY (ARRAY['residential', 'unclassified', 'road', 'minor']) THEN 11
+                WHEN class = 'railway' THEN 12                
+                ELSE 99
             END AS rank,
-	    tunnel
-       FROM import.road_labels_gen1
-      ORDER BY 
-           rank DESC;
+	        tunnel,
+            geometry
+    FROM road_labels_gen1
+    ORDER BY rank DESC;
 
 
-CREATE OR REPLACE VIEW import.brick_road_labels_gen0 AS 
-   SELECT osm_roads_gen0.class, osm_roads_gen0.type, regexp_replace(osm_roads_gen0.name::text, '(.*)\(.*\)'::text, '\1'::text) AS name,        
-           0 AS direction, osm_roads_gen0.geometry,
+CREATE OR REPLACE VIEW brick_road_labels_gen0 AS 
+    SELECT class, type, regexp_replace(name, '(.*)\(.*\)', '\1') AS name,        
+           0 AS direction,
            CASE
-               WHEN osm_roads_gen0.type::text = 'motorway'::text THEN 0::smallint
-               WHEN osm_roads_gen0.type::text = 'trunk'::text THEN 2::smallint
-               WHEN osm_roads_gen0.type::text = 'primary'::text THEN 5::smallint
-               ELSE 99::smallint
+               WHEN type = 'motorway' THEN 0
+               WHEN type = 'trunk' THEN 2
+               WHEN type = 'primary' THEN 5
+               ELSE 99
            END AS rank,
-	   tunnel
-      FROM import.road_labels_gen0
-     WHERE osm_roads_gen0.name IS NOT NULL AND osm_roads_gen0.name::text <> ''::text AND osm_roads_gen0.type::text IN ('motorway', 'trunk', 'primary')
-     ORDER BY 
-          rank DESC;       
+    	   tunnel,
+    	   geometry
+    FROM road_labels_gen0
+    ORDER BY rank DESC;       
 
 
-CREATE OR REPLACE VIEW import.brick_shields AS 
- SELECT osm_roads.class, osm_roads.type, 
-        CASE
-            WHEN osm_roads.ref::text ~ '^I ?\d+'::text THEN regexp_replace(osm_roads.ref::text, '^I ?(\d+).*'::text, 'I \1'::text)
-            WHEN osm_roads.ref::text ~ '^US ?\d+'::text THEN regexp_replace(osm_roads.ref::text, '^US ?(\d+).*'::text, 'US \1'::text)
-            WHEN osm_roads.ref::text ~ '^[[:alpha:]]+ ?\d+'::text THEN regexp_replace(osm_roads.ref::text, '^([[:alpha:]]+) ?(\d+).*'::text, '\1 \2'::text)
-            WHEN osm_roads.ref::text ~ '^[[:alpha:]]+-\d+'::text THEN regexp_replace(osm_roads.ref::text, '^([[:alpha:]]+)-(\d+).*'::text, '\1 \2'::text)
-            WHEN osm_roads.ref::text ~ '^\d+$'::text THEN osm_roads.ref::text
-            ELSE osm_roads.ref::text
-        END AS ref, length(osm_roads.ref::text) AS reflen, osm_roads.geometry
-   FROM import.osm_roads
-  WHERE osm_roads.ref IS NOT NULL AND osm_roads.ref::text <> ''::text AND (osm_roads.type::text = ANY (ARRAY['motorway'::character varying::text, 'trunk'::character varying::text, 'primary'::character varying::text, 'secondary'::character varying::text, 'tertiary'::character varying::text]))
-  ORDER BY 
-  	CASE WHEN type='motorway' THEN 0
-	     WHEN type='trunk' THEN 1
-	     WHEN type='primary' THEN 2
-	     WHEN type='secondary' THEN 3
-	     WHEN type='tertiary' THEN 4
-	     ELSE 99
-	END ASC;
+CREATE OR REPLACE VIEW brick_shields AS 
+    SELECT  class, type, 
+            CASE
+                WHEN ref ~ '^I ?\d+' THEN regexp_replace(ref, '^I ?(\d+).*', 'I \1')
+                WHEN ref ~ '^US ?\d+' THEN regexp_replace(ref, '^US ?(\d+).*', 'US \1')
+                WHEN ref ~ '^[[:alpha:]]+ ?\d+' THEN regexp_replace(ref, '^([[:alpha:]]+) ?(\d+).*', '\1 \2')
+                WHEN ref ~ '^[[:alpha:]]+-\d+' THEN regexp_replace(ref, '^([[:alpha:]]+)-(\d+).*', '\1 \2')
+                WHEN ref ~ '^\d+$' THEN ref
+                ELSE ref
+            END AS ref, 
+            length(ref) AS reflen, 
+            geometry
+    FROM osm_roads
+    WHERE ref IS NOT NULL AND ref != '' AND (type = ANY (ARRAY['motorway', 'trunk', 'primary', 'secondary', 'tertiary']))
+    ORDER BY 
+      	CASE WHEN type='motorway' THEN 0
+	         WHEN type='trunk' THEN 1
+	         WHEN type='primary' THEN 2
+	         WHEN type='secondary' THEN 3
+	         WHEN type='tertiary' THEN 4
+	         ELSE 99
+	    END ASC;
 
 
 	
-CREATE OR REPLACE VIEW import.brick_shields_gen0 AS 
- SELECT foo.class, foo.type, foo.ref, length(foo.ref) AS reflen, foo.geometry
-   FROM ( SELECT osm_roads_gen1.class, osm_roads_gen1.type, 
-                CASE
-                    WHEN osm_roads_gen1.ref::text ~ '^I ?\d+'::text THEN regexp_replace(osm_roads_gen1.ref::text, '^I ?(\d+).*'::text, 'I \1'::text)
-                    WHEN osm_roads_gen1.ref::text ~ '^US ?\d+'::text THEN regexp_replace(osm_roads_gen1.ref::text, '^US ?(\d+).*'::text, 'US \1'::text)
-                    WHEN osm_roads_gen1.ref::text ~ '^[[:alpha:]]+ ?\d+'::text THEN regexp_replace(osm_roads_gen1.ref::text, '^([[:alpha:]]+) ?(\d+).*'::text, '\1 \2'::text)
-                    WHEN osm_roads_gen1.ref::text ~ '^[[:alpha:]]+-\d+'::text THEN regexp_replace(osm_roads_gen1.ref::text, '^([[:alpha:]]+)-(\d+).*'::text, '\1 \2'::text)
-                    WHEN osm_roads_gen1.ref::text ~ '^\d+$'::text THEN osm_roads_gen1.ref::text
-                    ELSE osm_roads_gen1.ref::text
-                END AS ref, osm_roads_gen1.geometry
-           FROM import.osm_roads_gen1
-          WHERE osm_roads_gen1.ref IS NOT NULL AND osm_roads_gen1.ref::text <> ''::text AND (osm_roads_gen1.type::text = ANY (ARRAY['motorway'::character varying::text, 'trunk'::character varying::text, 'primary'::character varying::text, 'secondary'::character varying::text, 'tertiary'::character varying::text]))) foo
- ORDER BY
+CREATE OR REPLACE VIEW brick_shields_gen0 AS 
+    SELECT class, type, ref, length(ref) AS reflen, geometry
+    FROM ( SELECT    class, type, 
+                    CASE
+                        WHEN ref ~ '^I ?\d+' THEN regexp_replace(ref, '^I ?(\d+).*', 'I \1')
+                        WHEN ref ~ '^US ?\d+' THEN regexp_replace(ref, '^US ?(\d+).*', 'US \1')
+                        WHEN ref ~ '^[[:alpha:]]+ ?\d+' THEN regexp_replace(ref, '^([[:alpha:]]+) ?(\d+).*', '\1 \2')
+                        WHEN ref ~ '^[[:alpha:]]+-\d+' THEN regexp_replace(ref, '^([[:alpha:]]+)-(\d+).*', '\1 \2')
+                        WHEN ref ~ '^\d+$' THEN ref
+                        ELSE ref
+                    END AS ref, 
+                    geometry
+           FROM osm_roads_gen1
+           WHERE ref IS NOT NULL AND ref != '' AND (type = ANY (ARRAY['motorway', 'trunk', 'primary', 'secondary', 'tertiary']))) foo
+    ORDER BY
         CASE WHEN type='motorway' THEN 0
              WHEN type='trunk' THEN 1
              WHEN type='primary' THEN 2
