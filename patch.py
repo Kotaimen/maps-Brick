093fdb1ff720cb7f198a83bd9478154466781878
Kotaimen.Cao <kotaimen.c@gmail.com>
2013-11-26 15:51:30 +0800

    Add command line option and clear-label-cache

--- patch.py
+++ patch.py
@@ -3,17 +3,18 @@
 import xml.etree.cElementTree as ET
 import sys
 import os, os.path
-import subprocess 
+import subprocess
 import json
 import re
+import argparse
 
 def compile_mml(mml, xml):
     with open(xml, 'wb') as fp:
         subprocess.check_call(['carto', '-n', '-l', mml, ], stdout=fp)
-    
-def patch_xml(filename):    
+
+def patch_xml(filename):
     tree = ET.parse(filename)
-    
+
     # os.path.abspath() doesn't follow symbolic link
     base = os.path.join(os.getenv('PWD'), 'mapnik')
 
@@ -24,7 +25,7 @@ def patch_xml(filename):
     root.attrib['font-directory'] = 'font'
     root.attrib['base'] = base
 
-    
+
     print 'Patching map styles.'
     for style in root.findall('Style'):
         n = 0
@@ -46,11 +47,14 @@ def patch_xml(filename):
             if delete:
                 n += 1
                 style.remove(rule)
-        if n > 0: 
+        if n > 0:
             print '  ...deleted %d useless rules in style "%s"'% (n, style.attrib['name'])
 
     print 'Patching map layers.'
     for layer in root.findall('Layer'):
+        if layer.attrib['name'] == '10m_geographicline':
+            print '  ...set clear-label-cache'
+            layer.attrib['clear-label-cache'] = 'yes'
         # add "cache-features" option to layer if they contian more than one styles
         style_num = len(layer.findall('StyleName'))
         if style_num > 1:
@@ -59,22 +63,22 @@ def patch_xml(filename):
         elif style_num == 0:
             print '  ...delete empty layer "%s"' % layer.attrib['name']
             root.remove(layer)
-            
+
 
     print 'Overwiting %s' % filename
     tree.write(filename)
 
 
-def make_theme(theme):
+def make_theme(theme, smart=False):
     with open('mapnik/project.mml') as fp:
         base_mml = json.load(fp)
-        
+
     # generate default palette_all.xml
     print '>>>>> Theme "%s"' % theme
-    
-    def compile(stylesheets, xml_name):        
+
+    def compile(stylesheets, xml_name):
         base_mml['Stylesheet'] = stylesheets
-    
+
         mml_filename = 'mapnik/~project.mml'
         xml_filename = 'mapnik/xml/%s_%s.xml' % (theme, xml_name)
         with open(mml_filename, 'w') as fp:
@@ -89,19 +93,39 @@ def make_theme(theme):
             wfp.write(re.sub(r'@smart-halo:\s+\d+', '@smart-halo: 0', mss))
         with open('mapnik/~palette_halo.mss', 'w') as wfp:
             wfp.write(re.sub(r'@smart-halo:\s+\d+', '@smart-halo: 1', mss))
-    
-    compile([theme_palette, 'base.mss', 'road.mss', 'boundary.mss', 'label.mss'], 'all')
-    compile(['~palette.mss', 'base.mss'], 'base')
-    compile(['~palette.mss', 'road.mss', 'boundary.mss'], 'road')
-    compile(['~palette.mss', 'label.mss'], 'label')    
-    compile(['~palette_halo.mss', 'label.mss'], 'label_halo')        
+
+    if not smart:
+        compile([theme_palette, 'base.mss', 'road.mss', 'boundary.mss', 'label.mss'], 'all')
+    else:
+#     compile(['~palette.mss', 'base.mss', 'road.mss', 'boundary.mss'], 'base_road')
+        compile(['~palette.mss', 'base.mss'], 'base')
+        compile(['~palette.mss', 'road.mss', 'boundary.mss'], 'road')
+        compile(['~palette.mss', 'label.mss'], 'label')
+        compile(['~palette_halo.mss', 'label.mss'], 'label_halo')
 
 def main():
+    
     if not os.path.exists('mapnik/xml'):
         os.mkdir('mapnik/xml')
-    make_theme('brick')
-    make_theme('saga')    
-    # make_theme('moonlight')
+        
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('themes',
+                        type=str,
+                        nargs='+',
+                        metavar='themes',                        
+                        help='''theme names''',
+                       )
+    parser.add_argument('--smart',
+                        dest='smart',
+                        default=False,
+                        action='store_true',
+                        help='''generate smart halo style'''
+                       )                       
+    options = parser.parse_args()
+    
+    for theme in options.themes:
+        make_theme(theme, options.smart)
 
 
 if __name__ == '__main__':
