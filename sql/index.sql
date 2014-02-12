3eb5cabd24e6567ca743cfb04d4e793b6112f918
Kotaimen Cao <kotaimen.c@gmail.com>
2014-02-12 17:11:32 +0800

    Uncomment missing indexes

--- sql/index.sql
+++ sql/index.sql
@@ -1,27 +1,31 @@
+﻿BEGIN;
+
+SET search_path TO import, public;
+
 -- index
 CREATE INDEX ON osm_landusage_areas(type);
--- CREATE INDEX ON osm_landusage_areas(area);
+CREATE INDEX ON osm_landusage_areas(area);
 
 CREATE INDEX ON osm_landusages_gen0(type);
--- CREATE INDEX ON osm_landusages_gen0(area);
+CREATE INDEX ON osm_landusages_gen0(area);
 
 CREATE INDEX ON osm_landusages_gen1(type);
--- CREATE INDEX ON osm_landusages_gen1(area);
+CREATE INDEX ON osm_landusages_gen1(area);
 
 CREATE INDEX ON osm_admin(admin_level);
 
 
 CREATE INDEX ON osm_roads(type);
--- CREATE INDEX ON osm_roads(tunnel);
--- CREATE INDEX ON osm_roads(bridge);
--- CREATE INDEX ON osm_roads(oneway);
--- CREATE INDEX ON osm_roads(layer);
+CREATE INDEX ON osm_roads(tunnel);
+CREATE INDEX ON osm_roads(bridge);
+CREATE INDEX ON osm_roads(oneway);
+CREATE INDEX ON osm_roads(layer);
 
 CREATE INDEX ON osm_roads_gen0(type);
--- CREATE INDEX ON osm_roads_gen0(tunnel);
--- CREATE INDEX ON osm_roads_gen0(bridge);
--- CREATE INDEX ON osm_roads_gen0(oneway);
--- CREATE INDEX ON osm_roads_gen0(layer);
+CREATE INDEX ON osm_roads_gen0(tunnel);
+CREATE INDEX ON osm_roads_gen0(bridge);
+CREATE INDEX ON osm_roads_gen0(oneway);
+CREATE INDEX ON osm_roads_gen0(layer);
 
 CREATE INDEX ON osm_roads_gen1(type);
 -- CREATE INDEX ON osm_roads_gen1(tunnel);
@@ -47,3 +51,5 @@ CREATE INDEX ON osm_landusage_area_labels_gen1 USING gist((st_centroid(geometry)
 
 CREATE INDEX ON brick_shields USING GIST(geometry);
 CREATE INDEX ON brick_shields_gen0 USING GIST(geometry);
+
+COMMIT;
