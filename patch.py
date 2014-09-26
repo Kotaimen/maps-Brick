b331729262e2884b19a21fcf6f8c1c76a1d80646
Kotaimen <kotaimen.c@gmail.com>
2014-09-26 16:09:06 +0800

    - Remove unused layers - Remove styles with no rules - Set layer maxscale and min scale based on style ranges it uses

--- patch.py
+++ patch.py
@@ -2,6 +2,10 @@
 
 """Patch carto compiled mapnik style xml files for better load performance
 and for "smart halo" (aka variable depth haloing) rendering.
+
+This is ugly since its not likly portable between mapnik versions.
+
+Current targeting the still unreleased 2.3.x.
 """
 
 import xml.etree.cElementTree as ET
@@ -16,8 +20,10 @@ import time
 
 def compile_mml(mml, xml):
     """compile to mapnik xml using carto"""
+    print 'Compiling "%s" to "%s"...' % (mml, xml)
     with open(xml, 'wb') as fp:
         subprocess.check_call(['carto', '-n', '-l', mml, ], stdout=fp)
+    print '  ...done'
 
 
 def patch_xml(filename):
@@ -25,19 +31,29 @@ def patch_xml(filename):
     tree = ET.parse(filename)
 
     # os.path.abspath() doesn't follow symbolic link
-    base = os.path.join(os.getenv('PWD'), 'mapnik')
+    base = os.path.join(os.environ['PWD'], 'mapnik')
 
     print 'Patching map attributes.'
     print '  ...set base to:', base
     root = tree.getroot()
-
     root.attrib['font-directory'] = 'font'
     root.attrib['base'] = base
+    
+    # return tree
 
-    print 'Patching map styles.'
+    styles = {}
+
     for style in root.findall('Style'):
-        n = 0
+        print 'Patching style "%s"...' % style.attrib['name']
+
+        useless_rules = 0
+        num_of_rules = 0
+
+        style_max_scales = []
+        style_min_scales = []
+
         for rule in style.findall('Rule'):
+            num_of_rules += 1
             delete = False
             # delete rules with stroke width = 0
             for line in rule.findall(
@@ -46,6 +62,12 @@ def patch_xml(filename):
                 break
             # delete rule with empty text/shield name field
             for text in rule.findall('''.//TextSymbolizer'''):
+                try:
+                    if text.attrib['placement-type'] == 'dummy':
+                        # delete dummy placements to disable mapnik load warning
+                        del text.attrib['placements']
+                except KeyError:
+                    pass
                 if text.text == '':
                     delete = True
                     break
@@ -53,31 +75,65 @@ def patch_xml(filename):
                 if shield.text == '':
                     delete = True
                     break
-            if delete:
-                n += 1
-                style.remove(rule)
-        if n > 0:
-            print '  ...deleted %d useless rules in style "%s"' % (
-                n, style.attrib['name'])
+            for max_scale in rule.findall('MaxScaleDenominator'):
+                style_max_scales.append(int(max_scale.text))
+                break
+            for min_scale in rule.findall('MinScaleDenominator'):
+                style_min_scales.append(int(min_scale.text))
+                break
 
-    print 'Patching map layers.'
+            if delete:
+                useless_rules += 1
+                style.remove(rule)
+        else:
+            if num_of_rules == 0:
+                print '  ...deleted empty style'
+                root.remove(style)
+        if useless_rules > 0:
+            print '  ...deleted %d useless rules' % useless_rules
+
+        styles[style.attrib[
+            'name']] = style_max_scales, style_min_scales, num_of_rules
+
     for layer in root.findall('Layer'):
-        
-        # if layer.attrib['name'] == '10m_geographicline':
-        # print '  ...set clear-label-cache'
-        # layer.attrib['clear-label-cache'] = 'yes'
-        
-        style_num = len(layer.findall('StyleName'))
-        
+        print 'Patching layer "%s"...' % layer.attrib['name']
+        # counting styles&rules in a layer
+        rule_num = 0
+        style_num = 0
+        max_scale = 0
+        min_scale = 2 ** 32 - 1
+
+        for style_name in layer.findall('StyleName'):
+            style_max_scales, style_min_scales, num_of_rules = styles[
+                style_name.text]
+            rule_num += num_of_rules
+            style_num += 1
+            if style_max_scales:
+                max_scale = max(max_scale, max(style_max_scales))
+            if style_min_scales:
+                min_scale = min(min_scale, min(style_min_scales))
+        # limit layer scale range
+        if max_scale > 0:
+            print  '  ...limit max scale to', max_scale
+            layer.attrib['maxzoom'] = str(max_scale)
+        if min_scale < 2 * 32 - 1:
+            print  '  ...limit min scale to', min_scale
+
+            layer.attrib['minzoom'] = str(min_scale)
+
+
         # enable async postgis render
         datasource = layer.find('Datasource')
-        
+
         # apply async postgis read for speed
         for parameter in datasource.findall('Parameter'):
-            if parameter.attrib['name'] == 'type' and parameter.text == 'postgis':
-                print '  ...applying async postgis 3/5'
-                new_params = {'max_size': '5', 'asynchronous_request': 'true',
-                    'max_async_connection': '3'}
+            if parameter.attrib['name'] == 'type' and \
+                            parameter.text == 'postgis':
+                new_params = {
+                    'max_size': '10',
+                    'asynchronous_request': 'true',
+                    'max_async_connection': '5', }
+                print '  ...applying async postgis options'
                 for k, v in new_params.iteritems():
                     p = ET.Element('Parameter')
                     p.attrib['name'] = k
@@ -85,24 +141,24 @@ def patch_xml(filename):
                     datasource.append(p)
                 break
 
-        # add "cache-features"  to layer if they contain more than one styles
+        # add "cache-features" to layer if they contain more than one styles
         if style_num > 1:
-            print '  ...enable cache-features for layer "%s"' % layer.attrib[
-                'name']
+            print '  ...enable cache-features'
             layer.attrib['cache-features'] = 'yes'
-        elif style_num == 0:
-            print '  ...delete empty layer "%s"' % layer.attrib['name']
-            root.remove(layer)
 
-    print 'Overwiting %s' % filename
-    tree.write(filename)
+        # and delete any layers without any rules
+        if rule_num == 0:
+            print '  ...delete useless layer'
+            root.remove(layer)
+            
+    return tree
 
 
 def make_theme(theme, smart=False):
     with open('mapnik/project.mml') as fp:
         base_mml = json.load(fp)
 
-    print '>>>>> Patching theme "%s"' % theme
+    print '>>>>> Patching theme "%s" <<<<<' % theme
 
     def compile(stylesheets, xml_name):
         base_mml['Stylesheet'] = stylesheets
@@ -112,38 +168,43 @@ def make_theme(theme, smart=False):
         with open(mml_filename, 'w') as fp:
             json.dump(base_mml, fp, indent=2)
         compile_mml(mml_filename, xml_filename)
-        patch_xml(xml_filename)
-    
+        tree = patch_xml(xml_filename)
+        print 'Overwiting %s' % xml_filename
+        tree.write(xml_filename)
+
+
     theme_palette = 'palette.%s.mss' % theme
     with open(os.path.join('mapnik', theme_palette), 'r') as fp:
         mss = fp.read()
         with open('mapnik/~palette.mss', 'w') as wfp:
             out = re.sub(r'@scale-factor:\s+\d+', '@scale-factor: 1', mss)
             out = re.sub(r'@smart-halo:\s+\d+', '@smart-halo: 0', out)
-            out = re.sub(r'@default-halo:\s+\d+', '@default-halo: 0', out)   
+            out = re.sub(r'@default-halo:\s+\d+', '@default-halo: 0', out)
             wfp.write(out)
         with open('mapnik/~palette_halo.mss', 'w') as wfp:
             out = re.sub(r'@scale-factor:\s+\d+', '@scale-factor: 1', mss)
-#            out = re.sub(r'@smart-halo:\s+\d+', '@smart-halo: 1', mss)
-#            out = re.sub(r'@default-halo:\s+\d+', '@default-halo: 1', out)
+            # out = re.sub(r'@smart-halo:\s+\d+', '@smart-halo: 1', mss)
+            # out = re.sub(r'@default-halo:\s+\d+', '@default-halo: 1', out)
             wfp.write(out)
         with open('mapnik/~palette@2x.mss', 'w') as wfp:
-            out = re.sub(r'@scale-factor:\s+\d+', '@scale-factor: 2', mss)  
+            out = re.sub(r'@scale-factor:\s+\d+', '@scale-factor: 2', mss)
             out = re.sub(r'@smart-halo:\s+\d+', '@smart-halo: 0', out)
             out = re.sub(r'@default-halo:\s+\d+', '@default-halo: 0', out)
             wfp.write(out)
         with open('mapnik/~palette@2x_halo.mss', 'w') as wfp:
-            out = re.sub(r'@scale-factor:\s+\d+', '@scale-factor: 2', mss)  
-#            out = re.sub(r'@smart-halo:\s+\d+', '@smart-halo: 1', mss)
-#            out = re.sub(r'@default-halo:\s+\d+', '@default-halo: 1', out)
+            out = re.sub(r'@scale-factor:\s+\d+', '@scale-factor: 2', mss)
+            # out = re.sub(r'@smart-halo:\s+\d+', '@smart-halo: 1', mss)
+            # out = re.sub(r'@default-halo:\s+\d+', '@default-halo: 1', out)
             wfp.write(out)
 
     if not smart:
         compile(
-            ['~palette_halo.mss', 'base.mss', 'road.mss', 'bound.mss', 'label.mss'],
+            ['~palette_halo.mss', 'base.mss', 'road.mss', 'bound.mss',
+             'label.mss'],
             '_all')
         compile(
-            ['~palette@2x_halo.mss', 'base.mss', 'road.mss', 'bound.mss', 'label.mss'],
+            ['~palette@2x_halo.mss', 'base.mss', 'road.mss', 'bound.mss',
+             'label.mss'],
             '@2x_all')
     else:
         compile(['~palette.mss', 'base.mss'], '_base')
@@ -154,10 +215,8 @@ def make_theme(theme, smart=False):
         compile(['~palette@2x.mss', 'road.mss', 'bound.mss'], '@2x_road')
         compile(['~palette@2x.mss', 'label.mss'], '@2x_label')
         compile(['~palette@2x_halo.mss', 'label.mss'], '@2x_label_halo')
-    
-    
+
     print '>>>>> Theme "%s" patched' % theme
-    
 
 
 def main():
